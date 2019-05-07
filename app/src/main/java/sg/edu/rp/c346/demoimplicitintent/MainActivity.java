8e85cbbd3c8e936354aeede6427db214b1373189
17010247 <17010247@myrp.edu.sg>
2019-05-07 12:14:56 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/c346/demoimplicitintent/MainActivity.java
@@ -0,0 +1,36 @@
+package sg.edu.rp.c346.demoimplicitintent;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btnEmail;
+    EditText editTextMessage;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        editTextMessage = findViewById(R.id.editTextMessage);
+        btnEmail = findViewById(R.id.buttonEmail);
+
+        btnEmail.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent email = new Intent(Intent.ACTION_SEND);
+                email.putExtra(Intent.EXTRA_EMAIL, new String[]{"jason_lim@rp.edu.sg"});
+                email.putExtra(Intent.EXTRA_SUBJECT, "Test Email from C347");
+                email.putExtra(Intent.EXTRA_TEXT, editTextMessage.getText());
+                email.setType("message/rfc822");
+                startActivity(Intent.createChooser(email, "Choose an Email client :"));
+
+            }
+        });
+    }
+}
